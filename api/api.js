b338e0d3f003106ebfbbfec5065f37e0e0f56c7b
FXHibon <djxf44@gmail.com>
2015-02-26 11:04:22 +0100

    + authentication api + handling aynch request to redis

--- api/api.js
+++ api/api.js
@@ -1,9 +1,9 @@
 module.exports = function (router) {
-
     /**
      * createClient(port, host)
      */
-    var redisClient = require('redis').createClient(6379, "127.0.0.1");
+    var redisClient = require('redis').createClient(6379, "192.168.248.129");
+    var async = require('async');
     var apiDebug = require('debug')('RedTweet:api');
     var redisDebug = require('debug')('RedTweet:redis');
 
@@ -20,27 +20,44 @@ module.exports = function (router) {
      */
     router.post('/auth', function (req, res, next) {
         var user = req.body;
-        redisClient.hget('users', user.name, apiDebug);
 
-        var status = 200;
         var objResponse = {};
+        var userId;
+        var status = 200;
 
-        // User not found
-        if (!userId) {
-            status = 401;
-            objResponse.cause = "name";
-        } else {
-            var password = redisClient.hget('user:' + userId, 'password', apiDebug);
-            if (password !== user.password) {
-                status = 401;
-                objResponse.cause = "password";
+        var getLogin = function (callback) {
+            redisClient.hget(['users', user.name], callback);
+        };
+
+        var getPassword = function (id, callback) {
+            if (id) {
+                userId = id;
+                redisClient.hget(['user:' + id, 'password'], callback);
             } else {
-                var cookie = redisClient.hget('user:' + userId, 'auth', apiDebug);
-                res.cookie('auth', cookie);
+                callback('name');
             }
-        }
+        };
 
-        res.status(status).json(objResponse);
+        var getCookie = function (password, callback) {
+            if (password && password === user.password) {
+                redisClient.hget('user:' + userId, 'auth', callback);
+            } else {
+                callback('password');
+            }
+        };
+
+        async.waterfall([getLogin, getPassword, getCookie], function (err, cookieVal) {
+            if (err) {
+                apiDebug("error: ", err);
+                status = 401;
+                objResponse.cause = err;
+            } else {
+                apiDebug("ok: cookie:", cookieVal);
+                status = 200;
+                res.cookie('auth', cookieVal);
+            }
+            res.status(status).json(objResponse);
+        });
     });
 
 };
